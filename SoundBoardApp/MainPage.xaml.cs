61dab3d3ed5dd3dd178e719b32ca5736b40ea621
PAAnisimoff <anisimenok@hotmail.com>
2017-12-24 17:39:09 +0300

    Готовый вариант

--- SoundBoardApp/MainPage.xaml.cs
+++ SoundBoardApp/MainPage.xaml.cs
@@ -5,14 +5,17 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
@@ -26,7 +29,7 @@ namespace SoundBoardApp
     {
 
         private ObservableCollection<Sound> Sounds;
-
+        private List<String> Suggestions;
         private List<MenuItems> MenuItems;
 
         public MainPage()
@@ -50,22 +53,33 @@ namespace SoundBoardApp
 
         private void BackBtn_Click(object sender, RoutedEventArgs e)
         {
+            goBack();
+        }
+
+        private void AutoSearchSuggestsBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (String.IsNullOrEmpty(sender.Text))
+                goBack();
+            SoundManager.GetAllSounds(Sounds);
+            Suggestions = Sounds.Where(p => p.Name.StartsWith(sender.Text)).Select(p => p.Name).ToList();
+            AutoSearchSuggestsBox.ItemsSource = Suggestions;
+        }
+
+        private void AutoSearchSuggestsBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            SoundManager.GetSoundsByName(Sounds, sender.Text);
+            CategoryTextBlock.Text = sender.Text;
+            MenuItemsListView.SelectedItem = null;
+            BackBtn.Visibility = Visibility.Visible;
+        }
+
+        private void goBack()
+        {
             SoundManager.GetAllSounds(Sounds);
             CategoryTextBlock.Text = "Все звуки";
             MenuItemsListView.SelectedItem = null;
             BackBtn.Visibility = Visibility.Collapsed;
         }
-
-        private void AutoSearchSuggestsBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-        {
-
-        }
-
-        private void AutoSearchSuggestsBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
-        {
-
-        }
-
 
         private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
         {
@@ -80,6 +94,35 @@ namespace SoundBoardApp
             CategoryTextBlock.Text = menuItem.Category.ToString();
             SoundManager.GetSoundsByCategory(Sounds, menuItem.Category);
             BackBtn.Visibility = Visibility.Visible;
+        }
+
+        private async void SoundGridView_Drop(object sender, DragEventArgs e)
+        {
+            if(e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if(items.Any())
+                {
+                    var storageFile = items[0] as StorageFile;
+                    var contentType = storageFile.ContentType;
+                    StorageFolder folder = ApplicationData.Current.LocalFolder; 
+                    if(contentType=="audio/wav" || contentType=="audio/mpeg")
+                    {
+                        StorageFile newFile = await storageFile.CopyAsync(folder, storageFile.Name, NameCollisionOption.GenerateUniqueName);
+                        MyMediaElement.SetSource(await storageFile.OpenAsync(FileAccessMode.Read), contentType);
+                        MyMediaElement.Play();
+                    }
+                }
+            }
+        }
+
+        private void SoundGridView_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "Перетащите чтобы создать свой звук и плитку";
+            e.DragUIOverride.IsCaptionVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            e.DragUIOverride.IsGlyphVisible = true;
         }
     }
 }
